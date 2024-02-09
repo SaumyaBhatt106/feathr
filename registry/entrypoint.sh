ea7912e707fe6b427f628af93139ed6e16a02729
Saumya Bhatt <108668549+SaumyaBhatt106@users.noreply.github.com>
2024-02-09 15:13:07 +0530

    Code review

--- registry/entrypoint.sh
+++ registry/entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+LISTENING_PORT=8000
+
 if [ "$ENABLE_RBAC" = "true" ]; then
 
     # Start the SQL registry server and RBAC proxy
     uvicorn sql-registry.main:app --host 0.0.0.0 --port 80 &
-    uvicorn access_control.main:app --host 0.0.0.0 --port 8000
+    uvicorn access_control.main:app --host 0.0.0.0 --port $LISTENING_PORT
 else
     # Start the SQL registry server without RBAC
-    uvicorn sql-registry.main:app --host 0.0.0.0 --port 8000
+    uvicorn sql-registry.main:app --host 0.0.0.0 --port $LISTENING_PORT
 fi
